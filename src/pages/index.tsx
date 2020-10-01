d3d2a65b3a2470308edef32d9ae6eaf81310f104
pedroksty <pedrotrab2132@gmail.com>
2020-09-30 21:56:21 -0300

    Add Global Styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-import Head from "next/head";
+import { Title } from "../styles/pages/Home";
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello world</h1>
+      <Title>Hello world</Title>
     </div>
   );
 }
